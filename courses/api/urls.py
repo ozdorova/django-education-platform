e05e337854bfc65babfd0106c6706297bacf65f5
Mikhail <mbold1911@yandex.ru>
2024-04-25 14:26:38 +0300

    add enroll to course viewset

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -13,7 +13,7 @@ router.register('courses', views.CourseViewSet)
 urlpatterns = [
     path('subjects/', views.SubjectListView.as_view(), name='suject_list'),
     path('subjects/<pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
-    path('courses/<pk>/enroll/',
-         views.CourseEnrollView.as_view(), name='course_enroll'),
+    # path('courses/<pk>/enroll/',
+    #      views.CourseEnrollView.as_view(), name='course_enroll'),
     path('', include(router.urls))
 ]
--- courses/api/views.py
+++ courses/api/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
+from rest_framework.decorators import action
 
 
 class SubjectListView(generics.ListAPIView):
@@ -23,20 +24,27 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    """API представление для записи студентов на курсы"""
+# class CourseEnrollView(APIView):
+#     """API представление для записи студентов на курсы"""
 
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+#     authentication_classes = [BasicAuthentication]
+#     permission_classes = [IsAuthenticated]
 
-    def post(self, request, pk, format=None):
-        """POST"""
-        course = get_object_or_404(Course, pk=pk)
-        course.students.add(request.user)
-        return Response({'enrolled': True})
+#     def post(self, request, pk, format=None):
+#         """POST"""
+#         course = get_object_or_404(Course, pk=pk)
+#         course.students.add(request.user)
+#         return Response({'enrolled': True})
 
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
-    """API представление для курса только для чтения """
+    """API viewset представление курсов только для чтения """
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(detail=True, methods=['post'], authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        """Функция записи студента на курс"""
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
