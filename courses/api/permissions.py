3aaf6e35baeada3dc032e5bdda9fc9aebe7fa30a
Mikhail <mbold1911@yandex.ru>
2024-04-25 14:41:26 +0300

    add permission enroll

---
+++ courses/api/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsEnrolled(BasePermission):
+    """Проверка подписан ли студент на курс"""
+
+    def has_object_permission(self, request, view, obj):
+        return obj.students.filter(id=request.user.id).exists()
