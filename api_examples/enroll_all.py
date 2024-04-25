d99fce329417186218c7dbbbc154b2f1c6a4eb61
Mikhail <mbold1911@yandex.ru>
2024-04-25 15:42:04 +0300

    add api example

---
+++ api_examples/enroll_all.py
@@ -0,0 +1,27 @@
+import requests
+
+username = 'test_enroll'
+password = 'savsav1234'
+
+base_url = 'http://127.0.0.1:8000/api/'
+
+
+# извлечь все курсы
+r = requests.get(f'{base_url}courses/')
+
+courses = r.json()
+
+
+available_courses = ', '.join([course['title'] for course in courses])
+print(f'Доступные курсы: {available_courses}')
+
+
+for course in courses:
+    course_id = course['id']
+    course_title = course['title']
+
+    r = requests.post(
+        f'{base_url}courses/{course_id}/enroll/', auth=(username, password))
+
+    if r.status_code == 200:
+        print(f'Студент {username} успешно записан на курс: {course_title}')
