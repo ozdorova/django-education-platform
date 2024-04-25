29431e112f64630e0d6dfe8e10e09e12f4531b75
Mikhail <mbold1911@yandex.ru>
2024-04-25 15:19:05 +0300

    add course view set and detail course api

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from courses.models import Subject, Course, Module
+from courses.models import Subject, Course, Module, Content
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -30,4 +30,47 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'subject', 'title', 'slug',
             'overview', 'created', 'owner', 'modules',
+        ]
+
+
+class ItemRelatedField(serializers.RelatedField):
+    """Сериализатор рендера содержимого"""
+
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    """Сериализатор содежимого модулей"""
+
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = [
+            'order', 'item',
+        ]
+
+
+class ModuleWithContentSerializer(serializers.ModelSerializer):
+    """Сериализатор модуля с содержимым"""
+
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = [
+            'order', 'title', 'description', 'contents',
+        ]
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    """Сериализатор Курса с содержимым"""
+
+    modules = ModuleWithContentSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            'id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules',
         ]
--- courses/api/views.py
+++ courses/api/views.py
@@ -8,6 +8,8 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import CourseWithContentsSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -48,3 +50,8 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True, methods=['get'], serializer_class=CourseWithContentsSerializer, authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        """Возвращает обьект Course, если студент записан на курс"""
+        return self.retrieve(request, *args, **kwargs)
