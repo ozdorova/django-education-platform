04dd0671942a3a4af577f26d5eadd4113179a1a8
Mikhail <mbold1911@yandex.ru>
2024-04-25 14:00:41 +0300

    add permissions to enroll api

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -8,4 +8,6 @@ app_name = 'courses'
 urlpatterns = [
     path('subjects/', views.SubjectListView.as_view(), name='suject_list'),
     path('subjects/<pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
+    path('courses/<pk>/enroll/',
+         views.CourseEnrollView.as_view(), name='course_enroll'),
 ]
--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,11 @@
 from rest_framework import generics
-from courses.models import Subject
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class SubjectListView(generics.ListAPIView):
@@ -15,3 +20,16 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+    """API представление для записи студентов на курсы"""
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk, format=None):
+        """POST"""
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
