596638466bf5b8bd0230679af4ed243d133282dd
Mikhail <mbold1911@yandex.ru>
2024-04-26 18:52:18 +0300

    add docker compose

--- educa/settings/base.py
+++ educa/settings/base.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'chat',
     'channels',
-    'livereload',  # liveserver
 ]
 
 MIDDLEWARE = [
--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -1,4 +1,5 @@
 # export DJANGO_SETTINGS_MODULE=educa.settings.prod
+import os
 from .base import *
 
 DEBUG = False
@@ -11,6 +12,16 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
+# for docker
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
