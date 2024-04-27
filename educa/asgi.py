e35e0587b0e895c067780a7ada27673a7ac9d564
Mikhail <mbold1911@yandex.ru>
2024-04-27 14:08:15 +0300

    add daphne to compose

--- educa/asgi.py
+++ educa/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter  # Маршрутизаторы
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
@@ -20,7 +21,9 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
-    'websocket': AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(chat.routing.websocket_urlpatterns)
+        )
     ),
 })
