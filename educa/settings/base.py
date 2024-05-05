d281ad603dda0a46d7df354c2bfe65f523b2e94d
root <root@vixzjobste.local>
2024-05-05 10:32:59 +0000

    modified settings

--- educa/settings/base.py
+++ educa/settings/base.py
@@ -166,7 +166,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ]
 }
 
